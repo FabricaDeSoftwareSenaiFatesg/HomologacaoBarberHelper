8f94e6b75a2f8dd442ea1ee4d3b8e523ca4f2fe6
Eduardo Lemes Ribeiro <67037375+Leksuz@users.noreply.github.com>
2023-03-29 18:43:38 -0300

    Ajustado projeto do frontend

---
+++ barber-helper-front/barber-helper-angular/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  constructor(private primengConfig: PrimeNGConfig) {
+
+  }
+
+  date: Date = new Date;
+
+  ngOnInit() {
+
+    this.primengConfig.ripple = true;
+
+    this.primengConfig.zIndex = {
+      modal: 1100,    // dialog, sidebar
+      overlay: 1000,  // dropdown, overlaypanel
+      menu: 1000,     // overlay menus
+      tooltip: 1100   // tooltip
+    };
+
+  }
+
+}
--- barber-helper-front/barber-helper-angular/src/app/app.module.ts
+++ barber-helper-front/barber-helper-angular/src/app/app.module.ts
@@ -1,21 +1,23 @@
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './view/home/home.component';
-import { LoginComponent } from './view/login/autenticacao/login.component';
+import { ButtonModule } from 'primeng/button';
+import { CalendarModule } from 'primeng/calendar';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
+    ButtonModule,
+    CalendarModule,
     FormsModule
   ],
   providers: [],
