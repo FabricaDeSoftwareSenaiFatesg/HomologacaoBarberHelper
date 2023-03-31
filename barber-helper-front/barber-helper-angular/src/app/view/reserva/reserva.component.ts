0a930c6553958a7fb76425aa95f0dbec6adf8b1a
Diogo Lima <diogo.lima@grupoitss.com.br>
2023-03-31 20:01:22 -0300

    adicionando horários disponíveis dto

--- barber-helper-front/barber-helper-angular/src/app/view/reserva/reserva.component.ts
+++ barber-helper-front/barber-helper-angular/src/app/view/reserva/reserva.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-reserva',
-  templateUrl: './reserva.component.html',
-  styleUrls: ['./reserva.component.css']
-})
-export class ReservaComponent {
-
-  date: Date = new Date();
-
-  consultarHorarios() {
-    console.log(this.date);
-
-  }
-}
+// import { Component } from '@angular/core';
+// import {ReservaService} from "./reserva.service";
+//
+// @Component({
+//   selector: 'app-reserva',
+//   templateUrl: './reserva.component.html',
+//   styleUrls: ['./reserva.component.css']
+// })
+// export class ReservaComponent {
+//
+//   constructor() {
+//     protected reservaService: ReservaService;
+//   }
+//
+//   date: Date = new Date();
+//
+//   consultarHorarios() {
+//
+//     ReservaService
+//
+//   }
+// }
