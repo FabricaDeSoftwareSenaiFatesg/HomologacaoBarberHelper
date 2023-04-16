6504b91a9d8d3d7e50d3d0c3b80cb807a8864c56
Eduardo Lemes Ribeiro <67037375+Leksuz@users.noreply.github.com>
2023-04-16 13:36:22 -0300

    Rotas

--- barber-helper-front/barber-helper-angular/src/app/app-routing.module.ts
+++ barber-helper-front/barber-helper-angular/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './view/home/home.component';
+import { LoginComponent } from './view/login/login.component';
+import { PessoaListagemComponent } from './view/pessoa/pessoa-listagem/pessoa-listagem.component';
+import { ReservaComponent } from './view/reserva/reserva.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'pessoa', component: PessoaListagemComponent },
+  { path: 'reserva', component: ReservaComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
