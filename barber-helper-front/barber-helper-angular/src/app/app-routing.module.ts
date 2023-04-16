25464d911f1cbe18fe1fa4dc603a5498d08ceb4c
Eduardo Lemes Ribeiro <67037375+Leksuz@users.noreply.github.com>
2023-04-16 13:48:50 -0300

    Criado arquivo para rotas

--- barber-helper-front/barber-helper-angular/src/app/app-routing.module.ts
+++ barber-helper-front/barber-helper-angular/src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './view/home/home.component';
 import { LoginComponent } from './view/login/login.component';
-import { PessoaListagemComponent } from './view/pessoa/pessoa-listagem/pessoa-listagem.component';
 import { ReservaComponent } from './view/reserva/reserva.component';
+import { PessoaRoutes } from './view/pessoa/pessoa-routes';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'pessoa', component: PessoaListagemComponent },
+  { path: 'pessoa', children: PessoaRoutes },
   { path: 'reserva', component: ReservaComponent }
 ];
 
